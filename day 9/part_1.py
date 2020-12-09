47b4e5c71874c39e6a01ea6b54326896afee33cc
VolVox99 <alirastegar123@gmail.com>
2020-12-09 13:04:38 -0800

    uploaded wrong one for part 1

--- day 9/part_1.py
+++ day 9/part_1.py
@@ -3,7 +3,6 @@ with open('input.txt') as f:
 
 nums = [int(num) for num in nums]
 
-
 def can_be_summed(ls, num):
     for n in ls:
         res = num - n
@@ -12,26 +11,6 @@ def can_be_summed(ls, num):
 
     return False
 
-
-def find_contiguous(nums, sol):
-    def find_sum(nums, start, end):
-        return sum(nums[start: end + 1])
-        
-    start = 0
-    increase = 1
-
-    while start + increase < len(nums):
-        output = find_sum(nums, start, start + increase)
-        if output == sol:
-            return nums[start:start + increase + 1]
-
-        if output > sol:
-            start += 1
-            increase = 1
-
-        increase += 1
-
-
 preamble = 25
 for i, num in enumerate(nums):
     if i <= preamble:
@@ -39,9 +18,7 @@ for i, num in enumerate(nums):
 
     preamble_nums = nums[i - preamble:i]
     if not can_be_summed(preamble_nums, num):
-        continguous_nums = find_contiguous(nums, num)
-        print(max(continguous_nums) + min(continguous_nums))
-        break
+        print(num)
 
     
 
