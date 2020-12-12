943fca827d694cf32ea4d8f0b2c375535d582d24
VolVox99 <alirastegar123@gmail.com>
2020-12-11 18:39:05 -0800

    optimized day 11 solutions, runtime speed cut by ~40%

--- day11/part_1.py
+++ day11/part_1.py
@@ -1,64 +1,68 @@
 with open('input.txt') as f:
     seats = f.read().splitlines()
 
-
 empty = 'L'
 occ = '#'
 floor = '.'
 
-
 max_row = len(seats)
 max_col = len(seats[0])
 
+def get_top(coords):
+    row, col = coords
+    if row > 0:
+        return [row - 1, col]
+
+def get_bottom(coords):
+    row, col = coords
+    if row < max_row - 1:
+        return [row + 1, col]
+
+def get_left(coords):
+    row, col = coords
+    if col > 0:
+        return [row, col - 1]
+
+def get_right(coords):
+    row, col = coords
+    if col < max_col - 1:
+        return [row, col + 1]
+
+def get_top_left(coords):
+    top = get_top(coords)
+    if not top:
+        return False
+    return get_left(top)
+
+def get_top_right(coords):
+    top = get_top(coords)
+    if not top:
+        return False
+    return get_right(top)
+
+def get_bottom_left(coords):
+    bottom = get_bottom(coords)
+    if not bottom:
+        return False
+    return get_left(bottom)
+
+def get_bottom_right(coords):
+    bottom = get_bottom(coords)
+    if not bottom:
+        return False
+    return get_right(bottom)
+
+funcs = [get_top, get_bottom, get_left, get_right, get_top_left, get_top_right, get_bottom_left, get_bottom_right
+]
+
 def get_adj(seats, coords):
-    def get_top(coords):
-        row, col = coords
-        if row > 0:
-            return [row - 1, col]
+    occ_count = 0
+    for func in funcs:
+        val = func(coords)
+        if val and seats[val[0]][val[1]] == occ:
+            occ_count += 1
 
-    def get_bottom(coords):
-        row, col = coords
-        if row < max_row - 1:
-            return [row + 1, col]
-
-    def get_left(coords):
-        row, col = coords
-        if col > 0:
-            return [row, col - 1]
-
-    def get_right(coords):
-        row, col = coords
-        if col < max_col - 1:
-            return [row, col + 1]
-
-    def get_top_left(coords):
-        top = get_top(coords)
-        if not top:
-            return False
-        return get_left(top)
-
-    def get_top_right(coords):
-        top = get_top(coords)
-        if not top:
-            return False
-        return get_right(top)
-
-    def get_bottom_left(coords):
-        bottom = get_bottom(coords)
-        if not bottom:
-            return False
-        return get_left(bottom)
-
-    def get_bottom_right(coords):
-        bottom = get_bottom(coords)
-        if not bottom:
-            return False
-        return get_right(bottom)
-
-    funcs = [get_top, get_bottom, get_left, get_right, get_top_left, get_top_right, get_bottom_left, get_bottom_right
-    ]
-    vals = [func(coords) for func in funcs]
-    return [seats[c[0]][c[1]] for c in vals if c]
+    return occ_count
 
 #you cant assing items in a string so im using this to circumvent that
 def copy_replace(arr, i, new):
@@ -78,7 +82,6 @@ def get_count_2d_arr(arr, item):
     count = 0
     for i in arr:
         count += i.count(item)
-
     return count
 
 
@@ -90,13 +93,13 @@ def model_round(seats):
             if seat == floor:
                 continue
 
-            adjs = get_adj(seats, (row, col))
+            occ_count = get_adj(seats, (row, col))
 
-            if seat == empty and adjs.count(occ) == 0:
+            if seat == empty and occ_count == 0:
                 new_seats[row] = copy_replace(new_seats[row], col, occ)
 
 
-            elif seat == occ and adjs.count(occ) >= 4:
+            elif seat == occ and occ_count >= 4:
                 new_seats[row] = copy_replace(new_seats[row], col, empty)
 
     return new_seats
--- day11/part_2.py
+++ day11/part_2.py
@@ -1,98 +1,100 @@
 with open('input.txt') as f:
     seats = f.read().splitlines()
 
-
 empty = 'L'
 occ = '#'
 floor = '.'
 seat_types = [empty, occ]
 
-
 max_row = len(seats)
 max_col = len(seats[0])
 
+def get_top(coords, first = True):
+    row, col = coords
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+    if row > 0:
+        return get_top([row - 1, col], False)
+
+def get_bottom(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if row < max_row - 1:
+        return get_bottom([row + 1, col], False)
+
+def get_left(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if col > 0:
+        return get_left([row, col - 1], False)
+
+def get_right(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if col < max_col - 1:
+        return get_right([row, col + 1], False)
+
+def get_top_left(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if row > 0 and col > 0:
+        return get_top_left([row - 1, col - 1], False)
+
+def get_top_right(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if row > 0 and col < max_col - 1:
+        return get_top_right([row - 1, col + 1], False)
+
+def get_bottom_left(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if row < max_row - 1 and col > 0:
+        return get_bottom_left([row + 1, col - 1], False)
+
+def get_bottom_right(coords, first = True):
+    row, col = coords
+
+    if not first and seats[row][col] in seat_types:
+        return [row, col]
+
+    if row < max_row - 1 and col < max_col - 1:
+        return get_bottom_right([row + 1, col + 1], False)
+
+
+funcs = [get_top, get_bottom, get_left, get_right, get_top_left, get_top_right, get_bottom_left, get_bottom_right
+]
+
 def get_visible(seats, coords):
+    occ_count = 0
+    for func in funcs:
+        val = func(coords)
+        if val and seats[val[0]][val[1]] == occ:
+            occ_count += 1
 
-    def get_top(coords, first = True):
-        row, col = coords
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-        if row > 0:
-            return get_top([row - 1, col], False)
-
-
-    def get_bottom(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if row < max_row - 1:
-            return get_bottom([row + 1, col], False)
-
-    def get_left(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if col > 0:
-            return get_left([row, col - 1], False)
-
-    def get_right(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if col < max_col - 1:
-            return get_right([row, col + 1], False)
-
-    def get_top_left(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if row > 0 and col > 0:
-            return get_top_left([row - 1, col - 1], False)
-
-    def get_top_right(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if row > 0 and col < max_col - 1:
-            return get_top_right([row - 1, col + 1], False)
-
-    def get_bottom_left(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if row < max_row - 1 and col > 0:
-            return get_bottom_left([row + 1, col - 1], False)
-
-    def get_bottom_right(coords, first = True):
-        row, col = coords
-
-        if not first and seats[row][col] in seat_types:
-            return [row, col]
-
-        if row < max_row - 1 and col < max_col - 1:
-            return get_bottom_right([row + 1, col + 1], False)
-
-    funcs = [get_top, get_bottom, get_left, get_right, get_top_left, get_top_right, get_bottom_left, get_bottom_right
-    ]
-    vals = [func(coords) for func in funcs]
-    return [seats[c[0]][c[1]] for c in vals if c]
+    return occ_count
    
 #you cant assing items in a string so im using this to circumvent that
 def copy_replace(arr, i, new):
     return [j if idx != i else new for idx, j in enumerate(arr)]
-
 
 def two_d_arr_equal(arr_1, arr_2):
     for x, y in zip(arr_1, arr_2):
@@ -108,7 +110,6 @@ def get_count_2d_arr(arr, item):
         count += i.count(item)
     return count
 
-
 def model_round(seats):
     new_seats = seats[::]
     for row in range(max_row):
@@ -117,12 +118,12 @@ def model_round(seats):
             if seat == floor:
                 continue
 
-            visible = get_visible(seats, (row, col))
+            occ_count = get_visible(seats, (row, col))
 
-            if seat == empty and visible.count(occ) == 0:
+            if seat == empty and occ_count == 0:
                 new_seats[row] = copy_replace(new_seats[row], col, occ)
 
-            elif seat == occ and visible.count(occ) >= 5:
+            elif seat == occ and occ_count >= 5:
                 new_seats[row] = copy_replace(new_seats[row], col, empty)
 
     return new_seats
@@ -130,13 +131,12 @@ def model_round(seats):
 
 def final(old, new):
         new = model_round(old)
-
-
         if two_d_arr_equal(old, new):
             return get_count_2d_arr(new, occ)
             
         return final(new, model_round(new))
 
 
-print(final(seats, model_round(seats)))
+print(final(seats, seats))
+
 
